ca8201c06fa5e8487571b9f00f7fd794020ad01e
faustvault <philipp@mdsolutions.ru>
2020-01-18 16:06:44 +0300

    wow, i'm actually making it

---
+++ docs/mds/safe-box.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<!--  Last Published: Tue May 16 2017 09:06:21 GMT+0000 (UTC)  -->
+<html data-wf-page="58ca81ccf27bbb4f3d04c6fe" data-wf-site="58c9996f26822cec461452d1">
+<head>
+  <meta charset="utf-8">
+  <title>Protected page</title>
+  <meta content="Protected page" property="og:title">
+  <meta content="width=device-width, initial-scale=1" name="viewport">
+  <link href="css/normalize.css" rel="stylesheet" type="text/css">
+  <link href="css/components.css" rel="stylesheet" type="text/css">
+  <link href="css/mds.css" rel="stylesheet" type="text/css">
+  <script src="https://ajax.googleapis.com/ajax/libs/webfont/1.4.7/webfont.js"></script>
+  <script type="text/javascript">
+    WebFont.load({
+      google: {
+        families: ["Varela Round:400","Ubuntu:300,300italic,400,400italic,500,500italic,700,700italic","Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Bitter:400,700,400italic","Lemon:regular"]
+      }
+    });
+  </script>
+  <script src="js/modernizr.js" type="text/javascript"></script>
+  <link href="images/favicon.gif" rel="shortcut icon" type="image/x-icon">
+  <link href="images/mds_bw_webclip.gif" rel="apple-touch-icon">
+  <style>#amo_brand{display:none!important;}</style>
+</head>
+<body>
+<script>var amo_social_button = {id: 3871, hash: "52fe6a3a350e5726136e77c579da7caf2ba4be8cb8ce7a07f74cf6a4423f1896", locale: "ru"};</script><script id="amo_social_button_script" async="async" src="https://gso.amocrm.ru/js/button.js"> </script>
+<script type="text/javascript">(function (w, d) {w.amo_pixel_token = 'p1Ip4iB8yqaEyXU9HPmcTPELFT2XyQsFpnlZil546EARfY1QJCTZWI/hwrhcq6B+';var s = document.createElement('script'), f = d.getElementsByTagName('script')[0];s.id = 'amo_pixel_js';s.type = 'text/javascript';s.async = true;s.src = 'https://piper.amocrm.ru/pixel/js/tracker/pixel.js?token=' + w.amo_pixel_token;f.parentNode.insertBefore(s, f);})(window, document);</script>
+<script>(function(w, d, s, h, id) { w.roistatProjectId = id; w.roistatHost = h; var p = d.location.protocol == "https:" ? "https://" : "http://"; var u = /^.*roistat_visit=[^;]+(.*)?$/.test(d.cookie) ? "/dist/module.js" : "/api/site/1.0/"+id+"/init"; var js = d.createElement(s); js.async = 1; js.src = p+h+u; var js2 = d.getElementsByTagName(s)[0]; js2.parentNode.insertBefore(js, js2);})(window, document, 'script', 'cloud.roistat.com', '2b73d9cfb30a7a6e0ebdc26df0626fcc');</script>
+  <div class="utility-page-wrap">
+    <div class="div-block-5">
+      <div class="utility-page-content w-form w-password-page">
+        <form action="/.wf_auth" class="utility-page-form w-password-page" method="post"><img class="image-2" src="images/animated_mds_white.svg">
+          <h2 class="heading-4 mds_main">Media Digital Solutions</h2>
+          <div class="div-block-6"></div>
+          <h2 class="heading-4 mds_main">Protected page</h2>
+	  <a>This is a test made for Self-Storage.</a>
+	  <br/>
+	  <a>Stay tuned!</a>
+          <!--div class="w-password-page">
+            <input type="hidden" name="path" value="<%WF_FORM_VALUE_PATH%>">
+            <input type="hidden" name="page" value="<%WF_FORM_VALUE_PAGE%>">
+          </div>
+          <input autofocus="autofocus" class="w-input w-password-page" maxlength="256" name="pass" placeholder="Enter your password" type="password">
+          <input class="w-button w-password-page" data-wait="Please wait..." type="submit" value="Submit"-->
+        </form>
+        <!--div class="w-form-fail w-password-page">
+          <div>Incorrect password. Please try again.</div>
+        </div>
+        <div class="w-password-page" style="display: none">
+          <script type="application/javascript">
+            (function _handlePasswordPageOnload() {
+            	  if (/[?&]e=1(&|$)/.test(document.location.search)) {
+            	    document.querySelector('.w-password-page.w-form-fail').style.display = 'block';
+            	  }
+            	})()
+          </script>
+        </div-->
+      </div>
+    </div>
+  </div>
+  <div class="bottomsection bottomsectionmain footer-section">
+    <div class="w-container">
+      <div class="w-row">
+        <div class="column w-col w-col-6 w-col-medium-6 w-col-small-6 w-col-tiny-6">
+          <div class="copyright">© 2018 MDS. All rights reserved.&nbsp;</div>
+        </div>
+        <div class="align-right bottomcontactmainpage w-clearfix w-col w-col-6 w-col-medium-6 w-col-small-6 w-col-tiny-6">
+          <a class="social-btn w-hidden-main w-hidden-medium w-hidden-small w-hidden-tiny w-inline-block" href="http://facebook.com"><img class="fb-image image" src="images/facebook-icon.svg">
+          </a>
+          <a class="social-btn w-inline-block" href="mailto:contact@mdsolutions.ru?subject=Hello"><img class="image-7" src="images/email-icon.svg">
+          </a>
+	  <a style="color:white; text-decoration:none" class="social-btn w-inline-block" href="callto:+74951401984"> +7 495 140 1984 </a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js" type="text/javascript"></script>
+  <script src="js/mds.js" type="text/javascript"></script>
+  <!-- [if lte IE 9]><script src="https://cdnjs.cloudflare.com/ajax/libs/placeholders/3.0.2/placeholders.min.js"></script><![endif] -->
+</body>
+</html>
--- lightning.md
+++ lightning.md
@@ -2,6 +2,22 @@
 marp: true
 ---
 
+# สวัสดีชาวโลก ⚡️
+
+---
+
+# สวัสดีชาวโลก ⚡️
+# (так и должно быть)
+
+---
+
+# สวัสดีชาวโลก
+# (⬆️ это — Привет, мир ⚡️)
+# (так и должно быть)
+
+
+---
+
 # ⚡️ Unexpected lightning talk v0.0.1
 ![The Whitest Kids U' Know - God Wants You to Wear a Hat](https://i.makeagif.com/media/5-07-2015/JfGpO1.gif) 
 Credits: wkuk, "God wants you to wear a hat — ⚡️⚡️⚡️" (c)
@@ -13,8 +29,8 @@ Credits: wkuk, "God wants you to wear a hat — ⚡️⚡️⚡️" (c)
 ```
 ---
 
-# Mary Lou
-aka @crnacura (twitter)
+# Mary Lou 
+aka @crnacura 💁‍♀️
 
 ![Mary Lou](https://pbs.twimg.com/profile_images/805913360625790977/V0AGc7jc_400x400.jpg)
 
@@ -27,8 +43,12 @@ https://tympanus.net/Development/DraggableImageStrip/
 
 ---
 
+
+
+---
+
 # Martin Kleppe
-aka @aemkei
+aka @aemkei 🤓
 
 ![Martin](https://pbs.twimg.com/profile_images/971269394193141761/4bPlSxyL_400x400.jpg)
 
@@ -39,14 +59,37 @@ https://aem1k.com/
 ## Martin Kleppe
 aka @aemkei
 
+Продвинутый пользователь твиттера. 🐦
+
+
 * FÌRE 🔥 - анимашка огня в js за 128 байтов
 https://aem1k.com/fire
-* http://www.jsfuck.com/
+* http://www.jsfuck.com/ говорит сам за себя ()
 * https://aem1k.com/world/ исполняемый js-глобус 🌍
 
-140byt.es/aemkei - more tweet-sized code
+
+---
+
+# Tim Holman
+aka @tholman 🦄
+![Tim Holman](https://pbs.twimg.com/profile_images/851553731149778946/VDKWjp2V_400x400.jpg)
+
+---
+# Tim Holman
+aka @tholman 🦄
+
+* У Тима очень полезный github-аккаунт
+https://github.com/tholman
+* ![Intense close-up](https://media.giphy.com/media/13cXQYDrCrdFU4/source.gif) 
+http://tholman.com/intense-images/
 
 
+
+---
+# Tim Holman
+aka @tholman 🦄
+
+# Masterpiece — Elevator.js — http://tholman.com/elevator.js/ 
 
 ---
 
